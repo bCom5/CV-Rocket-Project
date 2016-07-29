283d5e040e1e27360283b13db7afd9268aef3aea
Mark Noworolski <jmn@Marks-MBP-3.lan>
2016-07-29 12:59:54 -0700

    picamTest.py readability

--- src/picamTest.py
+++ src/picamTest.py
@@ -5,36 +5,6 @@ import cv2
 import Constants
 from Filter import Filter as f
 
-def piCamFilter(camera, stream):
-	e1 = cv2.getTickCount() # Starttime
-	camera.capture(stream, format='bgr')
-	frame = stream.array
-	#coolFrame = np.copy(frame) # Copies the frame
-	#filt = f(frame, consts=consts, display=False) # Filter Object
-	filt.image = frame
-	ret, filtered, imagey, contours, h = filt.getContours(cv2.CHAIN_APPROX_SIMPLE)
-	coolImage = filt.run(frame)
-	cv2.imshow('frame',coolImage)
-	e2 = cv2.getTickCount()
-	time = (e2 - e1) / cv2.getTickFrequency()
-	print time
-
-# Runs the filter until finished (press q)
-
-# filt = f(cv2.imread("img/balloon.jpg"), consts=Constants.VIDEOS_CONTOUR_FILTER_CONSTANTS_1, display=False) # Filter Object
-# with picamera.PiCamera() as camera:
-# 	camera.resolution = (640,480)
-# 	camera.framerate = 30
-# 	camera.shutter_speed = camera.exposure_speed
-# 	camera.exposure_mode = 'off'
-# 	g = camera.awb_gains
-# 	camera.awb_mode = 'off'
-# 	camera.awb_gains = g
-# 	with picamera.array.PiRGBArray(camera) as stream:
-# 		while True:
-# 			#break
-# 			piCamFilter(camera, stream)
-
 camera = picamera.PiCamera()
 camera.resolution = (640,480)
 camera.framerate = 60
